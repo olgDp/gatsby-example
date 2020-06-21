5d093851ab0f753d1899df8daa83f3abb113d4c9
Apple <apple@MacBook-Pro-Apple.local>
2020-06-21 17:38:22 +0300

    bg color changed to light grey

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,13 +2,16 @@ import React from "react"
 import NavBar from "../NavBar"
 import Footer from "../Footer"
 
+// Assets
+import cs from "./styles.module.scss"
+
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className={cs.layout}>
       <NavBar />
       {children}
       <Footer />
-    </>
+    </div>
   )
 }
 
